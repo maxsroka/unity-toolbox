adc0e71289671a899cfd9ec1778553f585461df2
Max <36799862+pixldev@users.noreply.github.com>
2022-08-07 23:19:54 +0200

    dynamic method names

--- src/codeLens.ts
+++ src/codeLens.ts
@@ -1,32 +1,52 @@
 import { CancellationToken, CodeLens, CodeLensProvider, Command, ProviderResult, TextDocument } from 'vscode';
+import { debug } from './extension';
+import * as messages from "./unity-messages.json";
 
 export class UnityMessageCodeLensProvider implements CodeLensProvider {
+    hasBehaviourExp = new RegExp(/.*class.*: *(Mono|Network)Behaviour/);
+    isVoidMethodExp = new RegExp(/void.*\(.*\)/);
+    isUnityMessageExp: RegExp;
+
+    constructor() {
+        let methodsNames = "";
+
+        for (let i = 0; i < messages.length; i++) {
+            const msg = messages[i];
+
+            methodsNames += msg.name;
+
+            if (i < messages.length - 1) {
+                methodsNames += "|";
+            }
+        }
+
+        this.isUnityMessageExp = new RegExp("void.*(" + methodsNames + ")\(.*\)");
+    }
+
     provideCodeLenses(document: TextDocument, token: CancellationToken): ProviderResult<CodeLens[]> {
-        console.time('codelense');
+        console.time('codelens');
         const list = [];
         const text = document.getText();
-        const hasBehaviourExp = new RegExp(/.*class.*: *(Mono|Network)Behaviour/);
 
-        if (!hasBehaviourExp.test(text)) return;
+        if (!this.hasBehaviourExp.test(text)) return;
 
-        const isVoidMethodExp = new RegExp(/void.*\(.*\)/);
-        const isUnityMessageExp = new RegExp(/void.*(Start|Update|FixedUpdate)\(.*\)/);
         const lines = text.split('\n');
 
         for (let i = 0; i < lines.length; i++) {
-            if (!lines[i].match(isVoidMethodExp)) continue;
-            if (!lines[i].match(isUnityMessageExp)) continue;
+            if (!lines[i].match(this.isVoidMethodExp)) continue;
+            if (!lines[i].match(this.isUnityMessageExp)) continue;
 
             let cmd: Command = {
                 command: "",
                 title: "$(symbol-method) Unity Message",
+                tooltip: "This method is called by Unity"
             }
 
             var line = document.lineAt(i);
             list.push(new CodeLens(line.range, cmd));
         }
 
-        console.timeEnd('codelense');
+        console.timeEnd('codelens');
         return list;
     }
 }
